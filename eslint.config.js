0894e06b1907b3ef134fb6a78fe6104d1e8aa0bc
yulialener <lener.yu@yandex.ru>
2024-08-03 18:32:31 +0700

    Add rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,18 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+    {languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-var": "error",
+            "semi": "error",
+            "indent": "error",
+            "no-multi-spaces": "error",
+            "space-in-parens": "error",
+            "no-multiple-empty-lines": "error",
+            "prefer-const": "error",
+            "no-use-before-define": "error"
+        }
+    }
 ];
